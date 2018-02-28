paulgibbs/buddypress-alt

commit b7680a4c6f372f1b856fb0b86e4b80054a17c36e

Templates: improvements to template loading methods.
git-svn-id: https://buddypress.svn.wordpress.org/trunk@11821 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

src/bp-templates/bp-legacy/buddypress-functions.php
@@ -803,15 +803,18 @@ function bp_legacy_theme_object_template_loader() {
 	if ( ! bp_current_action() )
 		bp_update_is_directory( true, bp_current_component() );
 
-	$template_part = $object . '/' . $object . '-loop';
-
 	// The template part can be overridden by the calling JS function.
-	if ( ! empty( $_POST['template'] ) ) {
-		$template_part = sanitize_option( 'upload_path', $_POST['template'] );
+	if ( ! empty( $_POST['template'] ) && 'groups/single/members' === $_POST['template'] && 'group_members' === $object ) {
+		$template_part = 'groups/single/members.php';
+	} else {
+		$template_part = $object . '/' . $object . '-loop.php';
 	}
 
-	// Locate the object template.
-	bp_get_template_part( $template_part );
+	$template_path = bp_locate_template( array( $template_part ), false );
+
+	$template_path = apply_filters( 'bp_legacy_object_template_path', $template_path );
+
+	load_template( $template_path );
 	exit();
 }
 

src/bp-templates/bp-nouveau/includes/ajax.php
@@ -87,15 +87,19 @@ function bp_nouveau_ajax_object_template_loader() {
 		bp_update_is_directory( true, bp_current_component() );
 	}
 
-	$template_part = $object . '/' . $object . '-loop';
-
 	// The template part can be overridden by the calling JS function.
-	if ( ! empty( $_POST['template'] ) ) {
-		$template_part = sanitize_option( 'upload_path', $_POST['template'] );
+	if ( ! empty( $_POST['template'] ) && 'groups/single/members' === $_POST['template'] && 'group_members' === $object ) {
+		$template_part = 'groups/single/members.php';
+	} else {
+		$template_part = $object . '/' . $object . '-loop.php';
 	}
 
 	ob_start();
-	bp_get_template_part( $template_part );
+
+	$template_path = bp_locate_template( array( $template_part ), false );
+	$template_path = apply_filters( 'bp_legacy_object_template_path', $template_path );
+
+	load_template( $template_path );
 	$result['contents'] = ob_get_contents();
 	ob_end_clean();
 

commit 314cd141f144b2b56b6aec70c7c21a3b1a82c7b6

Fix group members list AJAX pagination.
Correct broken AJAX pagination on group members list, introduced in r11821.

Props jdgrimes.

Fixes #7704.

git-svn-id: https://buddypress.svn.wordpress.org/trunk@11871 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

src/bp-templates/bp-legacy/buddypress-functions.php
@@ -806,7 +806,7 @@ function bp_legacy_theme_object_template_loader() {
 		bp_update_is_directory( true, bp_current_component() );
 
 	// The template part can be overridden by the calling JS function.
-	if ( ! empty( $_POST['template'] ) && 'groups/single/members' === $_POST['template'] && 'group_members' === $object ) {
+	if ( ! empty( $_POST['template'] ) && 'groups/single/members' === $_POST['template'] ) {
 		$template_part = 'groups/single/members.php';
 	} else {
 		$template_part = $object . '/' . $object . '-loop.php';