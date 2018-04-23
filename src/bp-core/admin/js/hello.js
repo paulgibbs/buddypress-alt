5dd0ee9dcca3002ccae16b4a1ad46f237ec976d0
Paul Gibbs <djpaul@gmail.com>
2018-04-23 21:38:45 +0100

    interim

--- src/bp-core/admin/js/hello.js
+++ src/bp-core/admin/js/hello.js
@@ -13,15 +13,36 @@
 		document.getElementById( 'bp-hello-container' ).style.display = '';
 	};
 
+	// Close modal if "X" or background is touched.
 	document.addEventListener( 'click', function( event ) {
 		var backdrop = document.getElementById( 'bp-hello-backdrop' ),
-			modal = document.getElementById( 'bp-hello-container' ),
-			backdrop_click = backdrop.contains( event.target ),
-			modal_click = modal.contains( event.target ),
-			modal_close = event.target.classList.contains( 'close-modal' );
+			modal    = document.getElementById( 'bp-hello-container' );
 
-		// Close modal if background is touched.
+		if ( ! backdrop || ! modal ) {
+			return;
+		}
+
+		var backdrop_click = backdrop.contains( event.target ),
+			modal_click    = modal.contains( event.target ),
+			modal_close    = event.target.classList.contains( 'close-modal' );
+
 		if ( modal_close || backdrop_click || ! modal_click ) {
+			modal.parentNode.removeChild( modal );
+			backdrop.parentNode.removeChild( backdrop );
+			window.history.pushState( {}, '', document.location.href.split( '?' )[0] );
+		}
+	}, false );
+
+	// Close modal if escape key is presssed.
+	document.addEventListener( 'keyup', function( event ) {
+		if ( event.keyCode == 27 ) {
+			var backdrop = document.getElementById( 'bp-hello-backdrop' ),
+				modal    = document.getElementById( 'bp-hello-container' );
+
+			if ( ! backdrop || ! modal ) {
+				return;
+			}
+
 			modal.parentNode.removeChild( modal );
 			backdrop.parentNode.removeChild( backdrop );
 			window.history.pushState( {}, '', document.location.href.split( '?' )[0] );
