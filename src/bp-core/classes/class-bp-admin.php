dfa4ddad54dbac72311bc88c0eb3a3155150f688
Paul Gibbs <djpaul@gmail.com>
2018-04-23 21:45:36 +0100

    interim

--- src/bp-core/classes/class-bp-admin.php
+++ src/bp-core/classes/class-bp-admin.php
@@ -583,7 +583,7 @@ class BP_Admin {
 
 				<h2>Meow Meow</h2>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-				<br />
+				<br /><br />
 			</div>
 
 			<div class="bp-hello-footer">
